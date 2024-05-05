00eca07891ce1390dc57153749c5b6c4ba07f65d
Bung-Adi <adiws@yandex.com>
2024-05-05 19:10:13 +0700

    New Update in Block Change

---
+++ SimpleScript/02.BlockChange/AdvanceDataType.lua
@@ -0,0 +1,27 @@
+-- make text lowercase with string.lower
+local function Lowercase(Word)
+	print(string.lower(Word))
+end
+
+-- count the lenght of tring with string.len
+local function Lenght(Word)
+	print(string.len(Word))
+end
+
+--[[
+there are actualy 
+Vector3.zero
+Vector3.one
+Vector3.xAxis
+Vector3.yAxis
+Vector3.zAxis
+Color3.fromRGB
+Color3.fromHEX
+Vector2.zero
+Vector2.one
+Vector2.xAxis
+Vector2.yAxis
+BrickColor.Random()
+BrickColor.pallete()
+Enum.Material:GetEnumItems()
+]]
