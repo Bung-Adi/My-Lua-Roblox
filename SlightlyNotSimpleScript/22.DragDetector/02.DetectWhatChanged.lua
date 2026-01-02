b83f11b0c11d681a2dbf1aca384db103562488a9
Bung-Adi <adiws@yandex.com>
2026-01-02 16:02:50 +0700

    update DrageDetect

--- SlightlyNotSimpleScript/22.DragDetector/02.DetectWhatChanged.lua
+++ SlightlyNotSimpleScript/22.DragDetector/02.DetectWhatChanged.lua
@@ -3,3 +3,5 @@ local DG = script.Parent.DragDetector
 DG:GetPropertyChangedSignal("DragFrame"):Connect(function()
 	print(DG.DragFrame.Position)
 end)
+
+-- ref : https://create.roblox.com/docs/ui/3D-drag-detectors
